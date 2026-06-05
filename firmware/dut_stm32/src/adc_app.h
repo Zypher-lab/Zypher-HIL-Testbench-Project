0aa9c117b757384d82c2e4b728bcfa9de40516bb
Abdelfattah225 <abdelfattahelazazy@gmail.com>
2026-06-05 11:13:47 +0300

    Add STM32 ADC threshold DUT behavior (#7)

---
+++ firmware/dut_stm32/src/adc_app.h
@@ -0,0 +1,8 @@
+
+#ifndef ADC_APP_H
+#define ADC_APP_H
+
+int adc_app_init(void);
+void adc_app_update(void);
+
+#endif
--- firmware/dut_stm32/src/main.c
+++ firmware/dut_stm32/src/main.c
@@ -1,45 +1,29 @@
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 
-/*
- * STM32 DUT DIO Application
- *
- * Behavior:
- * - BUTTON_IN = PB0
- * - LED_OUT   = PB1
- *
- * If PB0 is HIGH -> PB1 HIGH
- * If PB0 is LOW  -> PB1 LOW
- *
- * Testbench meaning:
- * - ESP32 simulates button signal on PB0
- * - ESP32 monitors DUT LED output from PB1
- */
+#include "dio_app.h"
+#include "adc_app.h"
 
-#define DUT_GPIO_NODE DT_NODELABEL(gpiob)
-
-#define BUTTON_IN_PIN 0
-#define LED_OUT_PIN 1
-
-static const struct device *gpio_b = DEVICE_DT_GET(DUT_GPIO_NODE);
-
-int main()
+int main(void)
 {
-    if (!device_is_ready(gpio_b))
-    {
-        printk("ERROR: GPIOB device is not ready\n");
+    printk("STM32 integrated DUT started\n");
+
+    if (dio_app_init() != 0) {
+        printk("ERROR: DIO app init failed\n");
         return 0;
     }
-    int ret = gpio_pin_configure(gpio_b,BUTTON_IN_PIN, GPIO_INPUT | GPIO_PULL_DOWN);
-    ret = gpio_pin_configure(gpio_b,LED_OUT_PIN, GPIO_OUTPUT_INACTIVE);
-    while (1)
-    {
-        int button_state = gpio_pin_get_raw(gpio_b,BUTTON_IN_PIN);
-        gpio_pin_set_raw(gpio_b,LED_OUT_PIN,button_state ? 1 : 0 );
-        k_msleep(10);
-        
+
+    if (adc_app_init() != 0) {
+        printk("ERROR: ADC app init failed\n");
+        return 0;
     }
-    
+
+    while (1) {
+        dio_app_update();
+        adc_app_update();
+
+        k_msleep(10);
+    }
+
+    return 0;
 }
