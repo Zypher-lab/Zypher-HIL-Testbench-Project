66d49d2a6dfd333b351888bf0fb5c3332fa1bb5e
Abdelfattah225 <abdelfattahelazazy@gmail.com>
2026-06-05 12:33:10 +0300

    Add DAC_OUT2 support to ESP32 DAC service (#9)

--- firmware/testbench_esp32/src/dac_service.c
+++ firmware/testbench_esp32/src/dac_service.c
@@ -6,7 +6,9 @@
 
 #define ESP32_DAC_NODE DT_NODELABEL(dac)
 
-#define DAC_OUT1_CHANNEL_ID 1
+#define DAC_OUT1_CHANNEL_ID 0  /* GPIO25 */
+#define DAC_OUT2_CHANNEL_ID 1  /* GPIO26 */
+
 #define DAC_RESOLUTION_BITS 8
 #define DAC_REFERENCE_MV    3300
 
@@ -28,7 +30,21 @@ static int clamp_mv(int mv)
 static int mv_to_raw_8bit(int mv)
 {
     mv = clamp_mv(mv);
+
     return (mv * 255) / DAC_REFERENCE_MV;
+}
+
+static int get_dac_channel_id(const char *channel_name)
+{
+    if (strcmp(channel_name, "DAC_OUT1") == 0) {
+        return DAC_OUT1_CHANNEL_ID;
+    }
+
+    if (strcmp(channel_name, "DAC_OUT2") == 0) {
+        return DAC_OUT2_CHANNEL_ID;
+    }
+
+    return -1;
 }
 
 int dac_service_init(void)
@@ -37,29 +53,41 @@ int dac_service_init(void)
         return -1;
     }
 
-    struct dac_channel_cfg channel_cfg = {
+    struct dac_channel_cfg dac_out1_cfg = {
         .channel_id = DAC_OUT1_CHANNEL_ID,
         .resolution = DAC_RESOLUTION_BITS,
     };
 
-    if (dac_channel_setup(dac_dev, &channel_cfg) < 0) {
+    struct dac_channel_cfg dac_out2_cfg = {
+        .channel_id = DAC_OUT2_CHANNEL_ID,
+        .resolution = DAC_RESOLUTION_BITS,
+    };
+
+    if (dac_channel_setup(dac_dev, &dac_out1_cfg) < 0) {
+        return -1;
+    }
+
+    if (dac_channel_setup(dac_dev, &dac_out2_cfg) < 0) {
         return -1;
     }
 
     dac_write_value(dac_dev, DAC_OUT1_CHANNEL_ID, 0);
+    dac_write_value(dac_dev, DAC_OUT2_CHANNEL_ID, 0);
 
     return 0;
 }
 
 bool dac_service_write_mv(const char *channel_name, int mv)
 {
-    if (strcmp(channel_name, "DAC_OUT1") != 0) {
+    int channel_id = get_dac_channel_id(channel_name);
+
+    if (channel_id < 0) {
         return false;
     }
 
     int raw = mv_to_raw_8bit(mv);
 
-    if (dac_write_value(dac_dev, DAC_OUT1_CHANNEL_ID, raw) < 0) {
+    if (dac_write_value(dac_dev, channel_id, raw) < 0) {
         return false;
     }
 
